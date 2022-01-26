ea481f45a6fdaa20d870f4c0841c01ec74af329f
MvO-343 <80961601+MvO-343@users.noreply.github.com>
2022-01-26 23:30:01 +0100

    Delete oefenen directory

--- oefenen/acount_verwijderen.php
+++
@@ -1,9 +0,0 @@
-<?php
-//verbinding maken met database
-$verbinding = new PDO("mysql:host=localhost;dbname=oefenen", "root", "");
-//"DELETE FROM `opnieuw` WHERE `opnieuw`.`Naam` = \'diesel\'"
-$sth = $verbinding->prepare("DELETE FROM `opnieuw` WHERE `opnieuw`.`Naam` =\"".$_GET['Naam']."\"");
-$sth->execute();
-
-header("Refresh: 0.1; URL=http://localhost/oefenen/acount_weergeven.php")
-?>
